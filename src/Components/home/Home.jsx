02671f13c3fe6466d96b5cbc8b3a7071e782cc3d
SONEUNJUNG <sandy131712@gmail.com>
2022-03-04 18:28:11 +0900

    :zap: 홈화면 애니메이션 추가
    홈화면 애니메이션 추가
    
    :zap: 홈화면 애니메이션 추가
    
    홈화면 애니메이션 추가

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -4,16 +4,21 @@ import styled, { css } from "styled-components";
 const Home = () => {
   return (
     <>
-      <ContentWrapper>
-        <Title Front>Developer ;</Title>
-      </ContentWrapper>
+      <MobileUnderline>
+        <ContentWrapper>
+          <Underline Front>
+            <Title>I’m FrontEnd</Title>
+          </Underline>
+          <Underline>
+            <Title>Developer ;</Title>
+          </Underline>
+        </ContentWrapper>
+      </MobileUnderline>
       <Body>
         <ImageWrapper>
           <Image src={`../images/Eundomi.png`}></Image>
         </ImageWrapper>
-        <ContentWrapper Front>
-          <Title>I’m FrontEnd</Title>
-        </ContentWrapper>
+
         <Wrapper>
           <Caution>
             이력서 또는 포트폴리오는 메뉴 또는 위의 버튼을 통해 확인해주세요.{" "}
@@ -35,48 +40,92 @@ const ImageWrapper = styled.div`
   transform: translate(-50%, -50%);
 `;
 const Image = styled.img`
-  height: 70vh;
+  height: 60vh;
 `;
 const ContentWrapper = styled.div`
-  position: absolute;
-  top: 16rem;
-  right: 14rem;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 10em;
+  position: relative;
+  top: 15rem;
+  @media screen and (max-width: 1300px) {
+    top: 0;
+    justify-content: center;
+    & > span:first-child {
+      margin-right: 20px;
+    }
+  }
+  @media screen and (max-width: 740px) {
+    width: max-content;
+  }
+`;
+const Underline = styled.span`
 
-  ${(props) =>
-    props.Front &&
-    css`
-      top: 16rem;
-      left: 14rem;
-    `}
+  @media screen and (min-width: 1300px) {
+    
+    background-image: linear-gradient(transparent 60%, #f8cd07 40%);
+    background-repeat: no-repeat;
+    background-size: 0;
+    display: inline;
+    transition: 0.5s ease;
+    :hover {
+      background-size: 100%;
+    }
 
-  @media screen and (max-width: 1600px) {
-    z-index: 20;
-    position: absolute;
-    top: 30rem;
-    right: 14rem;
     ${(props) =>
       props.Front &&
       css`
-        top: 16rem;
-        left: 14rem;
+        z-index: 20;
       `}
+  } 
   }
 `;
+
+const MobileUnderline = styled.span`
+  
+@media screen and (max-width: 1300px) {
+  top:16rem;
+  z-index: 20;
+  background-image: linear-gradient(transparent 60%, #f8cd07 40%);
+  background-repeat: no-repeat;
+  background-size: 0;
+  display:flex;
+  position:relative;
+  justify-content: center;
+  transition: 0.5s ease;
+  z-index: 20;
+  background-position: 20vw ;
+  
+  :hover {
+    background-size: 60vw;
+  }
+
+    ${(props) =>
+      props.Front &&
+      css`
+        z-index: 20;
+      `}
+  }
+  }
+  @media screen and (max-width: 740px) {
+    background-position: 18vw ;
+  }
+  
+  
+`;
+
 const Title = styled.h2`
   font-family: "GmarketSansBold";
-  font-size: 5rem;
+  font-size: 4.6vw;
   margin: 0;
   color: #fff;
   text-shadow: 0 0 0.3em #d0d0d0, 0 0 0.6em #d0d0d0;
-  ${(props) =>
-    props.main &&
-    css`
-      text-align: right;
-      font-size: 10rem;
-      padding: 0;
-      color: rgba(255, 255, 255, 0.9);
-      text-shadow: none;
-    `}
+  @media screen and (max-width: 1300px) {
+    font-size: 4vw;
+  }
+  @media screen and (max-width: 740px) {
+    width: max-content;
+  }
 `;
 
 const Caution = styled.p`
